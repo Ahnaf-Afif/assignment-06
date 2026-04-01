38c27e22b59a79683ec3858264c12622f35eaf43
Ahnaf-Afif <williamhens123@gmail.com>
2026-04-01 22:48:37 +0600

    fixed navbar final

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
       <div className="navbar bg-base-100 shadow-sm flex flex-col md:flex-row justify-between items-center">
-        <div className="navbar-start pl-20">
+        <div className="navbar-start pl-20 text-4xl md:text-xl font-bold">
           <a className=" btn-ghost text-xl">DigiTools</a>
         </div>
         <div className="navbar-center md:hidden lg:block hidden">
@@ -18,7 +18,7 @@ const Navbar = () => {
           </div>
         </div>
         <div className="navbar-end flex gap-4 items-center pr-20 ">
-          <FaCartShopping />
+          <FaCartShopping className="hidden md:block" />
           <p className="hidden md:block">Login</p>
           <button className="btn btn-primary rounded-full hidden md:block">
             Get Started
